aff384c959a883b829ff44c1484e13052174ca31
anatkir <anya1.ak29@gmail.com>
2019-09-27 10:42:00 +0300

    Fixes

--- MCAClient/src/app/app-routing.module.ts
+++ MCAClient/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { EventsComponent } from './events/events.component';
 import { EventsTabsComponent } from './events-tabs/events-tabs.component';
 import { UsersComponent } from './users/users.component';
 
